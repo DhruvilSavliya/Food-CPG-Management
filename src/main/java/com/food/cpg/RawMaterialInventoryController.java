aff331551bf96b96308ca4a77f6b213163575b14
Kartik Gevariya <kr210190@dal.ca>
2021-02-21 23:42:08 +0530

    CGW-9 Controllers and rounting setup for all modules.

---
+++ src/main/java/com/food/cpg/RawMaterialInventoryController.java
@@ -0,0 +1,16 @@
+package com.food.cpg;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * @author Kartik Gevariya
+ */
+@Controller
+public class RawMaterialInventoryController {
+
+    @GetMapping("/raw-materials-inventory")
+    public String showRawMaterialsInventory(){
+        return "raw-materials-inventory";
+    }
+}
