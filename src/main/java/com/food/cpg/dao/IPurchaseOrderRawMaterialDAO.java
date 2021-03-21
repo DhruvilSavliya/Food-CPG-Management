d0f5f224e3fff73ee026c8ddf32be52dc5ae0fd1
Kartik Gevariya <kr210190@dal.ca>
2021-03-21 13:18:34 +0530

    Refactored add purchase-order functionality.

--- src/main/java/com/food/cpg/dao/IPurchaseOrderRawMaterialDAO.java
+++
@@ -1,9 +0,0 @@
-package com.food.cpg.dao;
-
-import java.util.List;
-
-import com.food.cpg.models.PurchaseOrderRawMaterial;
-
-public interface IPurchaseOrderRawMaterialDAO {
-    void save(List<PurchaseOrderRawMaterial> purchaseOrderRawMaterials);
-}
