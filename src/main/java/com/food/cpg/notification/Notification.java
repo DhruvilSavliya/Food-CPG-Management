90d7e05398b833da9fc52e3b664c0b9a41ed59f9
Kartik Gevariya <kr210190@dal.ca>
2021-04-08 19:13:08 +0530

    Notification module enhanced to show in-app notifications to logged in user.

--- src/main/java/com/food/cpg/notification/Notification.java
+++ src/main/java/com/food/cpg/notification/Notification.java
@@ -1,12 +1,21 @@
 package com.food.cpg.notification;
 
 import java.sql.Timestamp;
+import java.time.Instant;
 import java.util.List;
+
+import org.apache.logging.log4j.util.Strings;
+
+import com.food.cpg.authentication.AuthenticationSessionDetails;
 
 public class Notification implements INotification {
     private final int userId;
     private final String content;
     private final Timestamp notificationDate;
+
+    public Notification() {
+        this(0, Strings.EMPTY, Timestamp.from(Instant.now()));
+    }
 
     public Notification(int userId, String content, Timestamp notificationDate) {
         this.userId = userId;
@@ -27,7 +36,8 @@ public class Notification implements INotification {
     }
 
     public List<INotification> getAll() {
-        return getPersistence().getAll(this.getUserId());
+        int loggedInManufacturerId = getLoggedInManufacturerId();
+        return getPersistence().getAll(loggedInManufacturerId);
     }
 
     public void send() {
@@ -36,5 +46,10 @@ public class Notification implements INotification {
 
     private INotificationPersistence getPersistence() {
         return NotificationFactory.instance().makeNotificationPersistence();
+    }
+
+    private int getLoggedInManufacturerId() {
+        AuthenticationSessionDetails authenticationSessionDetails = AuthenticationSessionDetails.getInstance();
+        return authenticationSessionDetails.getAuthenticatedUserId();
     }
 }
